4c01215fb5faa7730fa7b39cec8602a92faabce4
Zargham Shaukat <zargham.shaukat@bitbash.dev>
2026-07-25 00:54:21 +0500

    debug: test .js import

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,4 @@
-import './middleware/auth';
-
+import { y } from './bar.js';
 export default function handler(req: any, res: any) {
-  res.json({ status: 'auth-middleware-imported' });
+  res.json({ status: 'bar-imported', y });
 }
