94d142178696d0e0d11e8a92ed00b11929d063a5
Zargham Shaukat <zargham.shaukat@bitbash.dev>
2026-07-25 00:30:59 +0500

    debug: test basic handler before import

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,3 @@
-import { app } from '../server/src/index';
-
 export default function handler(req: any, res: any) {
-  const originalPath = req.query.path as string | undefined;
-  if (originalPath) {
-    req.url = '/api/' + originalPath;
-  }
-  return app(req, res);
+  res.json({ step: 'before-import' });
 }
